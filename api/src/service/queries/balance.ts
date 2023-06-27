f9fd4791178383d12f24272a7942cecb93931107
Diego Troitino <diego.troitino@lacador.digital>
2023-06-26 23:04:15 -0300

    fix: Ajuste filtro de datas

--- api/src/service/queries/balance.ts
+++ api/src/service/queries/balance.ts
@@ -14,11 +14,9 @@ export const getBalance = async (user_id: string, date: Date): Promise<number> =
         //TODO use date-fns to manipulate dates better
 
         const startDate = `${date.getFullYear()}-${(date.getMonth() + 1).toString().padStart(2, '0')}-${date.getDate().toString().padStart(2, '0')} 00:00:00`;
-        const endDate = `${date.getFullYear()}-${(date.getMonth() + 1).toString().padStart(2, '0')}-${date.getDate().toString().padStart(2, '0')} 11:59:59`;
+        const endDate = `${date.getFullYear()}-${(date.getMonth() + 1).toString().padStart(2, '0')}-${date.getDate().toString().padStart(2, '0')} 23:59:59`;
 
-        //TODO is not working the data filter
-        const [rows, fields] = await connection.execute("SELECT SUM(amount) as dayBalance FROM transactions WHERE user_id = ?", [user_id]);
-        //const [rows, fields] = await connection.execute("SELECT SUM(amount) as dayBalance FROM transactions WHERE user_id = ? and DATE_FORMAT(date, '%Y-%m-%d %H:%i:%s') between STR_TO_DATE(?, '%Y-%m-%d %H:%i:%s') AND STR_TO_DATE(?, '%Y-%m-%d %H:%i:%s')", [user_id, startDate, endDate]);
+        const [rows, fields] = await connection.execute("select SUM(amount) as dayBalance from cashflowdb.transactions where user_id = ? and  date between ? and ?;", [user_id, startDate, endDate]);
         
         console.log('ROWS:', rows);
         return (rows as RowDataPacket[])[0]?.dayBalance as number || 0
